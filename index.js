cb7f48f4c514b512a4a8d674689d580bd1526810
ChilRx <chillvireak@gmail.com>
2024-07-03 10:56:41 +0700

    added a helper function

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-import express from 'express'
-import { database } from './config.js'
-import {set,ref,push, update,child} from 'firebase/database'
-    
+import express from "express";
+import { database } from "./config.js";
+import { set, ref, push, update, child } from "firebase/database";
 
-const app = new express()
-app.use(express.json())
+const app = new express();
+app.use(express.json());
 
+app.post("/tallyhook", async (req, res) => {
+  try {
+    let content = req.body;
+    let responseKey = push(child(ref(database), "respones")).key;
 
+    await set(ref(database, "responses/" + responseKey), simplify(content));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("error storing data");
+  }
+});
 
-app.post("/tallyhook", async (req, res) =>{
-    try{
-       let content = req.body
-       let responseKey = push(child(ref(database),'respones')).key
+app.listen(3000, () => {
+  console.log("The server is running on port 3000");
+});
 
-       await set(ref(database,"responses/" + responseKey), {
-        content
-       })
+function simplify(tallyContent) {
+  const tallyFormfields = tallyContent.data.fields;
+  const output = {};
 
-     
+  tallyFormfields.forEach((fieldKey) => {
+    output[fieldKey.key] = fieldKey.value;
+  });
 
-    }
-    catch(err){
-        console.log(err)
-        res.status(500).send("error storing data")
-    }
-})
-
-app.listen(3000, ()=>{
-    console.log("The server is running on port 3000")
-})
-
-
-
+  return output;
+}
